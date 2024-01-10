8f1b2ee5c90bfafede306f769faa429e44c133d9
amiril2 <alayne@brooklynsteamcenter.org>
2024-01-10 10:53:51 -0500

    benchmark

--- todo.py
+++ todo.py
@@ -17,17 +17,19 @@ app = Flask(__name__)
 def index():
       if request.method == 'POST':
             new_todo = request.form["new_todo"]
+
             Cursor = connect.cursor()
             Cursor.execute(f"INSERT INTO `todos` (`description`) VALUES ('{new_todo}') ")
             Cursor.close()
             connect.commit()
+
       Cursor = connect.cursor()
-
-      Cursor.execute("SELECT * FROM `todos`")
-
+      Cursor.execute("SELECT * FROM `todos` ORDER BY `complete`")
       results = Cursor.fetchall()
       Cursor.close()
+
       return render_template("todo.html.jinja", ToDos=results)
+
 
 @app.route('/delete_todo/<int:todo_index>', methods=['POST'])
 def todo_delete(todo_index):
@@ -37,3 +39,18 @@ def todo_delete(todo_index):
       connect.commit()
       return redirect('/')
 
+@app.route('/complete_todo/<int:todo_index>', methods=['POST'])
+def complete_todo(todo_index):
+      Cursor = connect.cursor()
+      Cursor.execute(f"UPDATE `todos` SET `complete` = 1 WHERE `id` = {todo_index}")
+      Cursor.close()
+      connect.commit()
+      return redirect('/')
+
+@app.route('/uncomplete_todo/<int:todo_index>', methods=['POST'])
+def uncomplete_todo(todo_index):
+      Cursor = connect.cursor()
+      Cursor.execute(f"UPDATE `todos` SET `complete` = 0 WHERE `id` = {todo_index}")
+      Cursor.close()
+      connect.commit()
+      return redirect('/')
