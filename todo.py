70392330d669fd77160f2fdddcb9c16e121773c6
amiril2 <alayne@brooklynsteamcenter.org>
2024-01-11 10:51:02 -0500

    Final Commit

--- todo.py
+++ todo.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect
+from flask_httpauth import HTTPBasicAuth
 import pymysql
 import pymysql.cursors
 from pprint import pprint as print
+from werkzeug.security import generate_password_hash, check_password_hash
 
 connect = pymysql.connect(
     database="alayne_todos",
@@ -12,8 +14,31 @@ connect = pymysql.connect(
 )
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+users = {
+    "Amiri": generate_password_hash("supersecretpassword")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+    
+
+
+if __name__ == '__main__':
+    app.run()
 
 @app.route("/", methods=['GET', 'POST'])
+@auth.login_required
 def index():
       if request.method == 'POST':
             new_todo = request.form["new_todo"]
@@ -27,11 +52,10 @@ def index():
       Cursor.execute("SELECT * FROM `todos` ORDER BY `complete`")
       results = Cursor.fetchall()
       Cursor.close()
-
-      return render_template("todo.html.jinja", ToDos=results)
-
+      return render_template("todo.html.jinja", ToDos=results), "Hello, {}!".format(auth.username())
 
 @app.route('/delete_todo/<int:todo_index>', methods=['POST'])
+@auth.login_required
 def todo_delete(todo_index):
       Cursor = connect.cursor()
       Cursor.execute(f"DELETE FROM `todos` WHERE `id`= {todo_index}")
@@ -40,6 +64,7 @@ def todo_delete(todo_index):
       return redirect('/')
 
 @app.route('/complete_todo/<int:todo_index>', methods=['POST'])
+@auth.login_required
 def complete_todo(todo_index):
       Cursor = connect.cursor()
       Cursor.execute(f"UPDATE `todos` SET `complete` = 1 WHERE `id` = {todo_index}")
@@ -48,6 +73,7 @@ def complete_todo(todo_index):
       return redirect('/')
 
 @app.route('/uncomplete_todo/<int:todo_index>', methods=['POST'])
+@auth.login_required
 def uncomplete_todo(todo_index):
       Cursor = connect.cursor()
       Cursor.execute(f"UPDATE `todos` SET `complete` = 0 WHERE `id` = {todo_index}")
